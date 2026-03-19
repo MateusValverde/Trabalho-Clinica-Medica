5bc3f71fadf593a4e5040aa6a299ba6328f38f6c
Mateus Valverde <mateusvalverde@email.com>
2026-03-18 21:37:01 -0300

    Conectando o site ao servidor do Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  // Agora o seu site vai falar com o servidor real na nuvem!
+  baseURL: 'https://trabalho-clinica-medica.onrender.com/api'
 });
 
-// ESTA LINHA ABAIXO É OBRIGATÓRIA E DEVE SER EXATAMENTE ASSIM:
 export default api;
